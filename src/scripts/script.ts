1deec555bae1c9ca209c4ebea7154cec6ce1362f
-Dmitry <hohlov.03@inbox.ru>
2022-07-24 23:48:24 +0300

    ;

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,68 +1,68 @@
 window.oncontextmenu = (e) => { return false }
-let amount :number = Number(localStorage.getItem('amount'))
-let chance :number = Number(localStorage.getItem('chance'))
+let amount :number = Number(localStorage.getItem('amount'));
+let chance :number = Number(localStorage.getItem('chance'));
 
-if(amount === 0) amount = 11
-if(chance === 0) chance = 25
+if(amount === 0) amount = 11;
+if(chance === 0) chance = 25;
 
-let colors = ['white', 'blue', 'green', 'red', 'purple', 'yellow', 'pink', 'brown', 'orange']
+let colors = ['white', 'blue', 'green', 'red', 'purple', 'yellow', 'pink', 'brown', 'orange'];
 
 const code = () :void => {
 
-    const flag :HTMLCanvasElement|null = document.querySelector<HTMLCanvasElement>("#flag")
-    const ctxFlag :CanvasRenderingContext2D|null|undefined = flag?.getContext('2d')
+    const flag :HTMLCanvasElement|null = document.querySelector<HTMLCanvasElement>("#flag");
+    const ctxFlag :CanvasRenderingContext2D|null|undefined = flag?.getContext('2d');
 
-    const canvas :HTMLCanvasElement|null = document?.querySelector<HTMLCanvasElement>("#canvas")
+    const canvas :HTMLCanvasElement|null = document?.querySelector<HTMLCanvasElement>("#canvas");
     const ctx :CanvasRenderingContext2D|null|undefined = canvas?.getContext('2d');
 
     const creating = (figure :string, ctx :CanvasRenderingContext2D, color :string, x :number, y:number, length:number) :void => {
 
-        ctx.beginPath()
-        ctx.fillStyle = color
-        ctx.moveTo(x, y)
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.moveTo(x, y);
 
         if(figure === 'square'){
 
-            ctx.lineTo(length*2 + x, y)
-            ctx.lineTo(length*2 + x, y + length)
+            ctx.lineTo(length*2 + x, y);
+            ctx.lineTo(length*2 + x, y + length);
         }
         if(figure === 'flag'){
 
-            ctx.lineTo(length*2 + x, y + length/2)
+            ctx.lineTo(length*2 + x, y + length/2);
         }
 
-        ctx.lineTo(x, y + length)
-        ctx.lineTo(x, y)
-        ctx.stroke()
-        ctx.fill()
+        ctx.lineTo(x, y + length);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.fill();
     }
 
     const paintingCanvas = (y :number, x :number) :void =>{
 
         if(arrey_bomb[y][x] === `bomb`){
 
-            alert('БУУУМ!')
-            window.location.reload()
+            alert('БУУУМ!');
+            window.location.reload();
         }
 
         for (let c = 0; c < colors.length; c++)
             if(Number(arrey_bomb[y][x]) === c){
 
-                if(ctx) creating('square', ctx, colors[c], (x-1)*300/amount, (y-1)*150/amount, 150/amount)
+                if(ctx) creating('square', ctx, colors[c], (x-1)*300/amount, (y-1)*150/amount, 150/amount);
 
-                arrey_bomb[y][x] = `${colors[c]}`
+                arrey_bomb[y][x] = `${colors[c]}`;
 
-                let score = 0
+                let score = 0;
 
                 for(let f = 1; f < amount+1; f++)
                     for(let s = 1; s < amount+1; s++){
 
-                        if( Number.isInteger( Number(arrey_bomb[f][s])) ) score = 0
+                        if( Number.isInteger( Number(arrey_bomb[f][s])) ) score = 0;
 
-                        else score++
+                        else score++;
                     }
                 
-                if(score === amount**2) alert('Поздравляю! Ты умеешь играть!')
+                if(score === amount**2) alert('Поздравляю! Ты умеешь играть!');
             }
     }
     const white = (j :number, i :number) :void =>{
@@ -71,10 +71,10 @@ const code = () :void => {
 
                 if(arrey_bomb[ j + f ] [ i + s ] === '0'){
 
-                    paintingCanvas(j + f, i + s)
-                    white(j + f - 1, i + s - 1)
+                    paintingCanvas(j + f, i + s);
+                    white(j + f - 1, i + s - 1);
                 }
-                else paintingCanvas(j + f , i + s)
+                else paintingCanvas(j + f , i + s);
             }
         }
     }
@@ -95,72 +95,72 @@ const code = () :void => {
 
                             if(Number(arrey_bomb[ j + 1 ][ i + 1 ]) === 0){
 
-                                paintingCanvas(j + 1, i + 1)
-                                white(j, i)
+                                paintingCanvas(j + 1, i + 1);
+                                white(j, i);
                             }
-                            else paintingCanvas(j + 1, i + 1)
+                            else paintingCanvas(j + 1, i + 1);
 
                         }else if(mausclick === 'right' && (Number.isInteger(arrey_bomb[j+1][i+1]) || arrey_bomb[j+1][i+1] === 'bomb') ){
                             
-                            if(ctx) creating('flag', ctx, 'red', (i*300 + 60)/amount, (j*150 + 20)/amount, 100/amount)
+                            if(ctx) creating('flag', ctx, 'red', (i*300 + 60)/amount, (j*150 + 20)/amount, 100/amount);
                         }
     }
 
     const noFlags = () :void => {
 
-        if(ctxFlag) creating('flag', ctxFlag, 'grey', 36, 17, 100)
+        if(ctxFlag) creating('flag', ctxFlag, 'grey', 36, 17, 100);
 
-        if(flag) flag.onclick = () :void => newFlags()
-        if(canvas) canvas.onclick = (event :MouseEvent) :void => click('left', event)
+        if(flag) flag.onclick = () :void => newFlags();
+        if(canvas) canvas.onclick = (event :MouseEvent) :void => click('left', event);
     }
 
     const newFlags = () :void => {
 
-        if(ctxFlag) creating('flag', ctxFlag, 'red', 36, 17, 100)
+        if(ctxFlag) creating('flag', ctxFlag, 'red', 36, 17, 100);
 
-        if(flag) flag.onclick = () :void => noFlags()
-        if(canvas) canvas.onclick = (event :MouseEvent) :void => click('right', event)
+        if(flag) flag.onclick = () :void => noFlags();
+        if(canvas) canvas.onclick = (event :MouseEvent) :void => click('right', event);
     }
 
-    if(ctx) creating('square', ctx, 'grey', 0, 0, 800)//field of play
-    if(ctxFlag) creating('square', ctxFlag, 'grey', 0, 0, 800)//!!topright!!
-    if(ctxFlag) creating('flag', ctxFlag, 'grey', 36, 17, 100)//flag
+    if(ctx) creating('square', ctx, 'grey', 0, 0, 800);//field of play
+    if(ctxFlag) creating('square', ctxFlag, 'grey', 0, 0, 800);//!!topright!!
+    if(ctxFlag) creating('flag', ctxFlag, 'grey', 36, 17, 100);//flag
 
-    if(flag) flag.onclick = () :void => newFlags()
-    if(canvas) canvas.onclick = (event :MouseEvent) :void => click('left', event)
-    if(canvas) canvas.oncontextmenu = (event :MouseEvent) :void => click('right', event)
+    if(flag) flag.onclick = () :void => newFlags();
+    if(canvas) canvas.onclick = (event :MouseEvent) :void => click('left', event);
+    if(canvas) canvas.oncontextmenu = (event :MouseEvent) :void => click('right', event);
 
     if(ctx) 
         for(let f = 0; f < amount; f++){
-            ctx.beginPath()
-            ctx.moveTo(0, f*150/amount)
-            ctx.lineTo(800, f*150/amount)
-            ctx.stroke()
-            ctx.beginPath()
-            ctx.moveTo(f*300/amount, 0)
-            ctx.lineTo(f*300/amount, 800)
-            ctx.stroke()
+            ctx.beginPath();
+            ctx.moveTo(0, f*150/amount);
+            ctx.lineTo(800, f*150/amount);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(f*300/amount, 0);
+            ctx.lineTo(f*300/amount, 800);
+            ctx.stroke();
         }
 
-    let nonBombs = 0
-    let arrey_bomb :string[][] = []
+    let nonBombs = 0;
+    let arrey_bomb :string[][] = [];
 
-    for(let i = 0; i < amount + 2; i++) arrey_bomb[i] = []
+    for(let i = 0; i < amount + 2; i++) arrey_bomb[i] = [];
 
     for(let y = 1; y < amount + 1 ; y++)
         for(let x = 1; x < amount + 1 ; x++)
 
-            if( Math.floor(Math.random() * 100) < chance) arrey_bomb[y][x] = 'bomb'
+            if( Math.floor(Math.random() * 100) < chance) arrey_bomb[y][x] = 'bomb';
 
             else {
-                arrey_bomb[y][x] = "non"
-                nonBombs++
+                arrey_bomb[y][x] = "non";
+                nonBombs++;
             }
         
-    const HTMLbombs :HTMLElement|null = document?.querySelector<HTMLElement>('#bombs')
-    if(HTMLbombs) HTMLbombs.innerHTML = String(amount**2 - nonBombs)
+    const HTMLbombs :HTMLElement|null = document?.querySelector<HTMLElement>('#bombs');
+    if(HTMLbombs) HTMLbombs.innerHTML = String(amount**2 - nonBombs);
 
-    let threat :number = 0
+    let threat :number = 0;
 
     for(let y = 1; y < amount + 1; y++)
         for(let x = 1; x < amount + 1; x++)
@@ -169,21 +169,20 @@ const code = () :void => {
                 for(let f = -1; f < 2; f++){
                     for(let s = -1; s < 2; s++){
 
-                        if(arrey_bomb[y + f][x + s] === 'bomb') threat++
+                        if(arrey_bomb[y + f][x + s] === 'bomb') threat++;
                     }
                 }
-                arrey_bomb[y][x] = String(threat)
-                threat = 0
+                arrey_bomb[y][x] = String(threat);
+                threat = 0;
             }
-        
 }
 
 const setON = () :void =>{
 
-    const buttonSettings :HTMLButtonElement|null = document.querySelector<HTMLButtonElement>('#button_settings')
+    const buttonSettings :HTMLButtonElement|null = document.querySelector<HTMLButtonElement>('#button_settings');
     if(buttonSettings) buttonSettings.onclick = () :void => {
 
-        const game :HTMLElement|null = document.querySelector<HTMLElement>('#game')
+        const game :HTMLElement|null = document.querySelector<HTMLElement>('#game');
         if(game) game.innerHTML = 
         `
             <center> 
@@ -197,38 +196,39 @@ const setON = () :void =>{
                     <div id="name_chance">${chance}%</div>
                 </div> 
             </center> 
-        `
-        const InpAmount :HTMLInputElement|null = document.querySelector<HTMLInputElement>('#amount')
+        `;
+        const InpAmount :HTMLInputElement|null = document.querySelector<HTMLInputElement>('#amount');
+
         if(InpAmount) InpAmount.oninput = function<HTMLInputElement>() :void {
 
-            const InpNameAmount :HTMLElement|null = document.querySelector<HTMLElement>('#name_amount')
+            const InpNameAmount :HTMLElement|null = document.querySelector<HTMLElement>('#name_amount');
 
-            if(InpNameAmount) InpNameAmount.innerHTML = this.value+`X${this.value}`
+            if(InpNameAmount) InpNameAmount.innerHTML = this.value+`X${this.value}`;
 
-            localStorage.setItem('amount', this.value)
-            amount = Number(this.value)
-        }
+            localStorage.setItem('amount', this.value);
+            amount = Number(this.value);
+        };
 
-        const InpChance :HTMLInputElement|null = document.querySelector<HTMLInputElement>('#chance')
+        const InpChance :HTMLInputElement|null = document.querySelector<HTMLInputElement>('#chance');
         if(InpChance) InpChance.oninput = function<HTMLInputElement>() :void {
 
-            const InpNameChance :HTMLElement|null = document.querySelector<HTMLElement>('#name_chance')
+            const InpNameChance :HTMLElement|null = document.querySelector<HTMLElement>('#name_chance');
 
-            if(InpNameChance) InpNameChance.innerHTML = this.value+`X${this.value}`
+            if(InpNameChance) InpNameChance.innerHTML = this.value+`X${this.value}`;
 
-            localStorage.setItem('chance', this.value)
-            chance = Number(this.value)
-        }
-        setOFF()
+            localStorage.setItem('chance', this.value);
+            chance = Number(this.value);
+        };
+        setOFF();
     }
 }
 
 const setOFF = () :void =>{
 
-    const buttonSettings :HTMLButtonElement|null = document.querySelector<HTMLButtonElement>('#button_settings')
+    const buttonSettings :HTMLButtonElement|null = document.querySelector<HTMLButtonElement>('#button_settings');
     if(buttonSettings) buttonSettings.onclick = () :void => {
 
-        const game :HTMLElement|null = document.querySelector<HTMLElement>('#game')
+        const game :HTMLElement|null = document.querySelector<HTMLElement>('#game');
         if(game) game.innerHTML = 
         ` 
             <span id="spanish">
@@ -249,11 +249,11 @@ const setOFF = () :void =>{
             <span size="10" id='bombs'></span>
 
         `
-        code()
+        code();
         
-        setON()
+        setON();
     }
 }
 
-code()
-setON()
+code();
+setON();
